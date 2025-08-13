a11bff514d5ddd56e8dcb99c4a93cb0a50db661b
ripplevibes11 <iammutiullah@gmail.com>
2025-08-13 15:36:45 +0500

    My 3rd changes

--- ICP.BLL/obj/Debug/net8.0/ICP.BLL.AssemblyInfo.cs
+++ ICP.BLL/obj/Debug/net8.0/ICP.BLL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ICP.BLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15b0cdc63c1161a4557dd9ea3072c351442c3eee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39bede64bbb719f2294a468e132b5a502e5c95a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ICP.BLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ICP.BLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
